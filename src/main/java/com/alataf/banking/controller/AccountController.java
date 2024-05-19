ea070f16679026dabfe6dbb16a0b2e52595617aa
meAltf <alataf.dce.cs1@gmail.com>
2024-05-20 01:20:02 +0530

    create rest api for getAccount by id

--- src/main/java/com/alataf/banking/controller/AccountController.java
+++ src/main/java/com/alataf/banking/controller/AccountController.java
@@ -4,10 +4,7 @@ import com.alataf.banking.dto.AccountDto;
 import com.alataf.banking.service.AccountService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/accounts")
@@ -28,5 +25,12 @@ public class AccountController {
     @PostMapping("/addAccount")
     public ResponseEntity<AccountDto> addAccount(@RequestBody AccountDto accountDto){
         return new ResponseEntity<>(accountService.createAccount(accountDto), HttpStatus.CREATED);
+    }
+
+    //get Account REST API
+    @GetMapping("/getAccountById/{id}")
+    public ResponseEntity<AccountDto> getAccountById(@PathVariable Long id) {
+        AccountDto accountDto = accountService.getAccountById(id);
+        return ResponseEntity.ok(accountDto);
     }
 }
--- src/main/java/com/alataf/banking/service/AccountService.java
+++ src/main/java/com/alataf/banking/service/AccountService.java
@@ -5,4 +5,6 @@ import com.alataf.banking.dto.AccountDto;
 public interface AccountService {
 
     AccountDto createAccount(AccountDto accountDto);
+
+    AccountDto getAccountById(Long id);
 }
