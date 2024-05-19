a4f45d375489640fb46767da50130023bfcdaae9
meAltf <alataf.dce.cs1@gmail.com>
2024-05-19 23:59:51 +0530

    create impl for create an  account

--- src/main/java/com/alataf/banking/service/impl/AccountServiceImpl.java
+++ src/main/java/com/alataf/banking/service/impl/AccountServiceImpl.java
@@ -1,6 +1,8 @@
 package com.alataf.banking.service.impl;
 
 import com.alataf.banking.dto.AccountDto;
+import com.alataf.banking.entity.Account;
+import com.alataf.banking.mapper.AccountMapper;
 import com.alataf.banking.repository.AccountRepository;
 import com.alataf.banking.service.AccountService;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,8 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public AccountDto createAccount(AccountDto accountDto) {
-        return null;
+        Account account = AccountMapper.mapToAccount(accountDto);
+        Account savedAccount = accountRepository.save(account);
+        return AccountMapper.mapToAccountDto(savedAccount);
     }
 }
